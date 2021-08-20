02786433983d5101b85b5d71cc2302eb7d84f7c7
Luis Biedma <lbiedma17@gmail.com>
2021-08-20 15:20:02 -0300

    soltrinffil

---
+++ codigos/practico1/p1ej3.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def soltrinffil(A, b):
+    n = len(b)
+    x = b.copy()
+
+    # Para detectar el primer elemento distinto de cero podemos
+    # hacer un loop en el range(n) y parar cuando encontramos
+    # algo distinto, para luego arrancar desde ahí
+    for idx in range(n):
+        if b[idx] != 0:
+            j = idx
+            break
+
+    for i in range(j, n):
+        x[i] = (b[i] - A[i, :i] @ x[:i])/A[i,i]
+
+    return x
+
+# Para hacer el recorrido del arreglo en reversa (para triangular superior)
+# es posible usar reversed
+# for idx in reversed(range(n)):
+#     if b[idx] != 0:
+#         j = idx
+#         break
